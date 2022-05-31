4980146251165492e68467963a198dae43370662
mikemtzp <mikemp8226@gmail.com>
2022-05-31 15:55:59 -0500

    Fix linter errors

--- modules/render.js
+++ modules/render.js
@@ -1,10 +1,10 @@
-import Books, { bookList } from "./createbooks.js";
+import Books, { bookList } from './createbooks.js';
 
 export default class Render {
   static renderBooks() {
-    const bookSection = document.querySelector(".box");
-    const bookListElement = document.createElement("div");
-    bookListElement.id = "bookList";
+    const bookSection = document.querySelector('.box');
+    const bookListElement = document.createElement('div');
+    bookListElement.id = 'bookList';
     if (bookList) {
       bookList.forEach((book) => {
         const bookElem = new Books(book.title, book.author, book.id);
